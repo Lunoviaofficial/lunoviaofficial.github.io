a529ba78477a244af43394c37367f859cbf513b1
Lunoviaofficial <devilpunk499@gmail.com>
2026-01-24 05:05:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+    toggleBtn.textContent = "☀";
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+    toggleBtn.textContent = "☾";
+  }
+});
 
